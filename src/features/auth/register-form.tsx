d326391f47fa63ac0e329c6bf611198fd44bed0d
Huy Vũ <huyhy1811@gmail.com>
2024-10-17 11:28:23 +0700

    Initial commit

---
+++ src/features/auth/register-form.tsx
@@ -0,0 +1,59 @@
+import { Link, useSearchParams } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form } from '../../components/ui/form';
+import { Input } from '../../components/ui/input';
+import { useRegister, registerInputSchema } from '../../lib/auth';
+
+type RegisterFormProps = {
+	onSuccess: () => void;
+
+}
+
+export const RegisterForm = ({ onSuccess }: RegisterFormProps) => {
+	const { register, handleSubmit, formState, watch } = useForm({
+		resolver: zodResolver(registerInputSchema), // Sử dụng zod để validate form
+		shouldUnregister: true,
+	});
+
+	const passwordValue = watch('password'); // Lấy giá trị của trường password
+	const registering = useRegister({ onSuccess });
+	const [searchParams] = useSearchParams();
+	const redirectTo = searchParams.get('redirectTo');
+
+	return (
+		<div>
+			<Form
+				onSubmit={handleSubmit((values) => {
+					registering.mutate(values);
+				})}
+			>
+				{() => (
+					<>
+						<Input
+							type="email"
+							label="Email Address"
+							error={formState.errors['email']?.message as string | undefined}
+							registration={register('email', { required: 'Email is required' })}
+						/>
+						<Input
+							type="password"
+							label="Password"
+							error={formState.errors['password']?.message as string | undefined}
+							registration={register('password', { required: 'Password is required' })}
+						/>
+						<Input
+							type="password"
+							label="Confirm Password"
+							error={formState.errors['confirmPassword']?.message as string | undefined}
+							registration={register('confirmPassword', {
+								required: 'Confirm password is required',
+								validate: (value) => value === passwordValue || 'Passwords do not match',
+							})}
+						/>
+					</>
+				)}
+			</Form>
+		</div>
+	);
+}
--- src/features/register.tsx
+++ src/features/register.tsx
@@ -1,58 +1,21 @@
-
 import { useState } from 'react';
+import { RegisterForm } from './auth/register-form';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
-const RegisterForm = ({ onSwitchToLogin }: { onSwitchToLogin: () => void }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    
-    console.log('Registering with', email, password);
-  };
+export const RegisterRoute = () => {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const redirectTo = searchParams.get('redirectTo');
 
   return (
-    <form onSubmit={handleSubmit} className="register-form">
-      <p>Register</p>
-
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          
-        />
-      </div>
-      <div>
-        <label htmlFor="confirmPassword">Confirm Password</label>
-        <input
-          type="password"
-          id="confirmPassword"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          
-        />
-      </div>
-      <button type="submit">Register</button>
-
-      <a href="#" className="login-link" onClick={onSwitchToLogin}>
-        Back to Login
-      </a>
-    </form>
-  );
-};
-
-export default RegisterForm;
+    <>
+      <RegisterForm
+        onSuccess={() => {
+          navigate(`${redirectTo ? `${redirectTo}` : '/app'}`, {
+            replace: true,
+          })
+        }}
+      />
+    </>
+  )
+}
